3776c1ac8ea7c7ef56c51b16e3aaa56cedb56b4f
barabas <vitalii.shulenin@gmail.com>
2020-01-17 16:12:37 +0100

    SQL: added sql scripts

--- TG.Exam.SQL/DAL.cs
+++ TG.Exam.SQL/DAL.cs
@@ -11,7 +11,7 @@ namespace TG.Exam.SQL
 {
     public class DAL
     {
-        private SqlConnection GetConnection() 
+        private SqlConnection GetConnection()
         {
             var connectionString = ConfigurationManager.AppSettings["ConnectionString"];
 
@@ -53,7 +53,7 @@ namespace TG.Exam.SQL
 
         public DataTable GetAllOrders()
         {
-            var sql = String.Empty;
+            var sql = "select * from orders";
 
             var ds = GetData(sql);
 
@@ -63,7 +63,10 @@ namespace TG.Exam.SQL
         }
         public DataTable GetAllOrdersWithCustomers()
         {
-            var sql = String.Empty;
+            var sql = @"
+select o.OrderId, o.OrderDate, c.CustomerId, c.CustomerFirstName, c.CustomerLastName from orders as o
+join customers as c on c.CustomerId = o.OrderCustomerId
+";
 
             var ds = GetData(sql);
 
@@ -74,8 +77,14 @@ namespace TG.Exam.SQL
 
         public DataTable GetAllOrdersWithPriceUnder(int price)
         {
-            var sql = String.Empty;
-
+            var sql = $@"
+select o.*, r.CommonPrice from orders as o
+right join  
+	(select oi.OrderId, sum(oi.Count * i.ItemPrice) as CommonPrice from ordersitems as oi
+	join items as i on oi.itemid = i.itemid
+	group by oi.OrderId
+	having sum(oi.Count * i.ItemPrice) <= {price}) as r on r.orderid = o.orderid
+";
             var ds = GetData(sql);
 
             var result = ds.Tables.OfType<DataTable>().FirstOrDefault();
@@ -85,14 +94,41 @@ namespace TG.Exam.SQL
 
         public void DeleteCustomer(int orderId)
         {
-            var sql = String.Empty;
+            // 1. search customer id related to the order and put it into variable
+            // 2. remove all pairs order-item
+            // 3. remove all orders
+            // 4. remove customer
+            var sql = $@"
+declare @CustomerId int
+select @CustomerId = c.customerid from customers c
+inner join orders o 
+on o.ordercustomerid = c.customerid
+where o.orderid = {orderId}
+
+delete oi
+from orders o
+left join ordersitems oi 
+on oi.orderid = o.orderid
+where o.ordercustomerid = @CustomerId
+
+delete o from orders o
+where o.ordercustomerid = @CustomerId
+
+delete c from customers c
+where c.customerid = @CustomerId
+";
 
             Execute(sql);
         }
 
         internal DataTable GetAllItemsAndTheirOrdersCountIncludingTheItemsWithoutOrders()
         {
-            var sql = String.Empty;
+            var sql = @"
+select i.itemid, i.itemname, count(oi.Count) Count from items i
+left join ordersitems oi
+on i.itemid = oi.itemid
+group by i.itemid, i.itemname
+";
 
             var ds = GetData(sql);
 
