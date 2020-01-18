f7deb65b72ea933823358b37e7e034c2caf59ca3
barabas <vitalii.shulenin@gmail.com>
2020-01-18 23:33:29 +0100

    MVC: added controllers and views

---
+++ TG.Exam.WebMVC/Controllers/ApiUsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TestModels = TG.Exam.WebMVC.Models;
+
+namespace Salestech.Exam.WebMVC.Controllers
+{
+    [Route("api/users")]
+    public class ApiUsersController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> GetUsers()
+        {
+            var users = await Task.Run(() =>
+            {
+                var modifiedUsers = TestModels.User.GetAll();
+                modifiedUsers.ForEach(u => u.Age += 10);
+
+                return modifiedUsers;
+            });
+
+            return Json(users);
+        }
+    }
+}
---
+++ TG.Exam.WebMVC/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestModels = TG.Exam.WebMVC.Models;
+
+namespace TG.Exam.WebMVC.Controllers
+{
+    /// <summary>
+    /// Actually, I didn't catch how I should accomplish 1D, 2B, 3C options from description.
+    /// I did them as I understood.
+    /// </summary>
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var users = TestModels.User.GetAll();
+
+            return View(users);
+        }
+
+        public ActionResult Fetch(TestModels.User user)
+        {
+            return View(user);
+        }
+    }
+}
