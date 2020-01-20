82af5e51a36fc1036715880a644c37fb3faea038
barabas <vitalii.shulenin@gmail.com>
2020-01-20 11:13:20 +0100

    Algo: changed methods

--- TG.Exam.Algorithms/Program.cs
+++ TG.Exam.Algorithms/Program.cs
@@ -4,24 +4,27 @@ namespace TG.Exam.Algorithms
 {
     class Program
     {
+        // this method includes simple functionality, so no necessity to use recursion
+        // the same result can be achieved by straightforward loop
         static int Foo(int a, int b, int c)
         {
-            if (1 < c)
-                return Foo(b, b + a, c - 1);
-            else
-                return a;
+            for (var i = b; 1 < c; c--)
+            {
+                b += a;
+                a = i;
+                i = b;
+            }
+
+            return a;
         }
 
         static int[] Bar(int[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
-                for (int j = 0; j < arr.Length - 1; j++)
-                    if (arr[j] > arr[j + 1])
-                    {
-                        int t = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = t;
-                    }
+            // or just use Array.Sort(arr) instead of inventing bycicle
+            // this function is implementation of quick sorting with recursion
+            // it is much faster than usual bubble sorting
+            Sort(ref arr, 0, arr.Length - 1);
+
             return arr;
         }
 
@@ -31,6 +34,44 @@ namespace TG.Exam.Algorithms
             Console.WriteLine("Bar result: {0}", string.Join(", ", Bar(new[] { 7, 2, 8 })));
 
             Console.ReadKey();
+        }
+
+        private static void Sort(ref int[] arr, int leftIndex, int rightIndex)
+        {
+            if (leftIndex >= rightIndex)
+                return;
+
+            var partitionIndex = CalcPartitionIndex(ref arr, leftIndex, rightIndex);
+
+            Sort(ref arr, leftIndex, partitionIndex - 1);
+            Sort(ref arr, partitionIndex + 1, rightIndex);
+        }
+        private static int CalcPartitionIndex(ref int[] arr, int leftIndex, int rightIndex)
+        {
+            var pivot = arr[rightIndex];
+            var j = leftIndex;
+
+            for (int i = leftIndex; i <= rightIndex - 1; i++)
+            {
+                if (arr[i] > pivot)
+                    continue;
+
+                SwapElements(ref arr, j, i);
+                j++;
+            }
+
+            SwapElements(ref arr, j, rightIndex);
+
+            return j;
+        }
+        private static void SwapElements(ref int[] arr, int leftIndex, int rightIndex)
+        {
+            if (arr[leftIndex] == arr[rightIndex])
+                return;
+
+            int temp = arr[leftIndex];
+            arr[leftIndex] = arr[rightIndex];
+            arr[rightIndex] = temp;
         }
     }
 }
